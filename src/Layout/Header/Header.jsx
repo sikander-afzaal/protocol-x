6ae90893de51cf110ba5d263d1378deb646a660d
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-07 08:27:51 +0500

    first

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,49 +7,66 @@ import logo from "../../Assets/logo.svg";
 
 function Header() {
   const [open, setOpen] = useState(false);
+  const [bg, setBg] = useState(false);
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.scrollY > 100) {
+        setBg(true);
+      } else {
+        setBg(false);
+      }
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
   return (
-    <div className={styles.header}>
-      <div
-        onClick={() => setOpen(false)}
-        className={`${styles.overlay}  ${open ? styles.openOverlay : ""}`}
-      ></div>
-      <img src={logo} className={styles.logo} alt="" />
-      <div
-        className={`${styles.rightHeader}  ${open ? styles.openHeader : ""}`}
-      >
-        <FontAwesomeIcon
+    <div className={`${styles.headerCont}  ${bg ? styles.blackBg : ""}`}>
+      <div className={styles.header}>
+        <div
           onClick={() => setOpen(false)}
-          icon={faXmark}
+          className={`${styles.overlay}  ${open ? styles.openOverlay : ""}`}
+        ></div>
+        <img src={logo} className={styles.logo} alt="" />
+        <div
+          className={`${styles.rightHeader}  ${open ? styles.openHeader : ""}`}
+        >
+          <FontAwesomeIcon
+            onClick={() => setOpen(false)}
+            icon={faXmark}
+            className={styles.mobile}
+          />
+          <a href="#" className={styles.navLinks}>
+            Pre-sale
+          </a>
+          <a href="#" className={styles.navLinks}>
+            Whitepaper
+          </a>
+          <a href="#" className={styles.navLinks}>
+            About us
+          </a>
+          <a href="#" className={styles.navLinks}>
+            Contact us
+          </a>
+          <a href="#" className={styles.navLinks}>
+            chart
+          </a>
+          <a href="#" className={styles.navLinks}>
+            buy ptx
+          </a>
+          <a href="#" className={styles.navLinks}>
+            dex
+          </a>
+          <button className={styles.ctaBtn}>enter dapp</button>
+        </div>
+        <FontAwesomeIcon
+          onClick={() => setOpen(true)}
+          icon={faBars}
           className={styles.mobile}
         />
-        <a href="#" className={styles.navLinks}>
-          Pre-sale
-        </a>
-        <a href="#" className={styles.navLinks}>
-          Whitepaper
-        </a>
-        <a href="#" className={styles.navLinks}>
-          About us
-        </a>
-        <a href="#" className={styles.navLinks}>
-          Contact us
-        </a>
-        <a href="#" className={styles.navLinks}>
-          chart
-        </a>
-        <a href="#" className={styles.navLinks}>
-          buy ptx
-        </a>
-        <a href="#" className={styles.navLinks}>
-          dex
-        </a>
-        <button className={styles.ctaBtn}>enter dapp</button>
       </div>
-      <FontAwesomeIcon
-        onClick={() => setOpen(true)}
-        icon={faBars}
-        className={styles.mobile}
-      />
     </div>
   );
 }
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import circle from "../../../Assets/hero/circle.png";
+import shield from "../../../Assets/hero/shield.png";
+import icon from "../../../Assets/hero/icon.png";
 function Hero() {
-  return <div className={styles.hero}>Hero</div>;
+  return (
+    <div className={styles.cont}>
+      <div className={styles.hero}>
+        <div className={styles.leftHero}>
+          <h1>
+            THE FUTURE OF <br /> DECENTRALIZED <br /> FINANCE
+          </h1>
+          <p>
+            ProtocolX is here to change the game by implementing an
+            ever-developing, expanding, and growing ecosystem that all feeds
+            into each other.
+          </p>
+          <button className={styles.ctaBtn}>Buy $PTX</button>
+        </div>
+        <div className={styles.rightHero}>
+          <img src={shield} className={styles.sheild} alt="" />
+          <img src={icon} className={styles.icon} alt="" />
+        </div>
+        <img src={circle} className={styles.circle} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
